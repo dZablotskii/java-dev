c58f2c8a91cc33d1d03d3264c403bb914875ade0
dZablotskii <denisvirus@yandex.ru>
2014-10-15 20:37:18 +0700

    Enter name of file

--- JavaCodeEditor.java
+++ JavaCodeEditor.java
@@ -4,6 +4,8 @@ class JavaCodeEditor{
 	public static void main(String[]args)
 	  throws IOException	
 	{
-		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter name of file, which will change.");
+		String name = in.nextLine();	
 	}
 }
