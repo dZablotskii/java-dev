0025068af242b43eadee51905a3af794b32380bc
dZablotskii <denisvirus@yandex.ru>
2014-10-15 20:50:14 +0700

    Function of edit code

--- JavaCodeEditor.java
+++ JavaCodeEditor.java
@@ -8,6 +8,17 @@ class JavaCodeEditor{
 		System.out.println("Enter name of file, which will change.");
 		String name = in.nextLine();
 		BufferedReader br = new BufferedReader(new FileReader(name));
-		String code = br.readLine();	
+		String code = br.readLine();
+		code = 	edit(code);
+	}
+	public static String edit(String code){
+		code = code.replaceAll(";", "; \n ");
+		code = code.replaceAll("\\{", "\\{ \n ");
+		code = code.replaceAll("\\(", " \\( ");
+		code = code.replaceAll("\\)", " \\) ");
+ 		code = code.replaceAll("\\+", " \\+ ");
+		code = code.replaceAll("\\-", " \\- ");
+		code = code.replaceAll("\\*", " \\* ");
+		return code;
 	}
 }
